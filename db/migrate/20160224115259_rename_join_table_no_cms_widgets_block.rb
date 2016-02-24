53b6f467ad8952a3e2b9b02051d356e95753612a
Luis Mendo <luis.mendo@the-cocktail.com>
2016-02-24 13:17:56 +0100

    Fix join table name.

---
+++ db/migrate/20160224115259_rename_join_table_no_cms_widgets_block.rb
@@ -0,0 +1,7 @@
+class RenameJoinTableNoCmsWidgetsBlock < ActiveRecord::Migration
+  def self.up
+    if ActiveRecord::Base.connection.table_exists? 'no_cms_blocks_blocks_no_cms_widgets_widgets'
+      rename_table :no_cms_blocks_blocks_no_cms_widgets_widgets, :no_cms_blocks_blocks_widgets_widgets
+    end
+  end
+end
